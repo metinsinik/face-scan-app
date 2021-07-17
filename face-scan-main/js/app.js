300cc0daf99d39c1e6e0fd02b38c181a288ccfd6
metinsinik <44985367+metinsinik@users.noreply.github.com>
2021-07-17 22:33:15 +0200

    first commit

---
+++ face-scan-main/js/app.js
@@ -0,0 +1,31 @@
+function detection() {
+  $(document).ready(function () {
+    $(".image").faceDetection({
+      complete: function (faces) {
+        for (let i = 0; i < faces.length; i++) {
+          $("<div>", {
+            class: "face",
+
+            css: {
+              left: faces[i].x * faces[i].scaleX + "px",
+              top: faces[i].y * faces[i].scaleY + "px",
+              width: faces[i].width * faces[i].scaleX + "px",
+              height: faces[i].height * faces[i].scaleX + "px",
+            },
+          }).insertAfter(this);
+        }
+      },
+      error: function (code, message) {},
+    });
+  });
+}
+
+let btn = document.querySelector("container");
+
+btn.addEventListener("click", () => {
+  if (container.style.display === "none") {
+    container.style.display = "block";
+  } else {
+    container.style.display = "none";
+  }
+});
